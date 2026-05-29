28f74d8b7245c1c1450127e6af1c6b7d2ef5c4cd
Vimalraj J <vimalraj.j@tech9.com>
2026-05-29 22:12:36 +0530

    Feature: Find max profit and all solutions, display results in UI   - Build a function to find max profit and all solutions using Depth First Search (DFS)

--- index.js
+++ index.js
@@ -1,4 +1,51 @@
-function findSolutions() {
-  const value = document.getElementById('input')?.value;
-  console.log(value);
+function getSolutions(value) {
+  const buildings = [
+    { name: "T", time: 5, profit: 1500 },
+    { name: "P", time: 4, profit: 1000 },
+    { name: "C", time: 10, profit: 2000 },
+  ];
+
+  let maxProfit = 0;
+  let answers = [];
+
+  function dfs(currentTime, totalProfit, counts) {
+    let canBuild = false;
+
+    for (const b of buildings) {
+      const finishTime = currentTime + b.time;
+
+      if (finishTime <= value) {
+        canBuild = true;
+
+        const earned =
+          (value - finishTime) * b.profit;
+
+        counts[b.name]++;
+
+        dfs(
+          finishTime,
+          totalProfit + earned,
+          counts
+        );
+
+        counts[b.name]--;
+      }
+    }
+
+    if (!canBuild) {
+      if (totalProfit > maxProfit) {
+        maxProfit = totalProfit;
+        answers = [{ ...counts }];
+      } else if (totalProfit === maxProfit) {
+        answers.push({ ...counts });
+      }
+    }
+  }
+
+  dfs(0, 0, { T: 0, P: 0, C: 0 });
+
+  return {
+    maxProfit,
+    answers,
+  };
 }
